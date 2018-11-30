32ca57fcdcb4db1d80f16a643c68ed50e02f9a42
Takafumi Tamura <taktamur@gmail.com>
2018-11-30 21:35:21 +0900

    Adapterの名前を変更 レイアウトxmlで、layoutManagerとadapter(DataBinding)を繋げるように変更

--- app/src/main/java/jp/paming/positionedphoto/MainActivity.kt
+++ app/src/main/java/jp/paming/positionedphoto/MainActivity.kt
@@ -1,11 +1,9 @@
 package jp.paming.positionedphoto
 
-import android.content.ContentResolver
 import android.content.Context
 import android.databinding.DataBindingUtil
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.support.v7.widget.GridLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.util.Log
 import android.view.LayoutInflater
@@ -16,6 +14,7 @@ import kotlinx.android.synthetic.main.activity_main.*
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.widget.Toast
+import jp.paming.positionedphoto.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
@@ -67,13 +66,11 @@ class MainActivity : AppCompatActivity() {
             }
         }
         Log.d("photoDataList_loc","$photoDataList")
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
 
-        recycleView.also {
-            it.layoutManager = GridLayoutManager(this, 2)
-            it.adapter = MyRecycleAdapter(this).also {
-                it.onClick = ::onClick
-                it.list = photoDataList
-            }
+        binding.adapter = ItemAdapter(this).also {
+            it.onClick = ::onClick
+            it.list = photoDataList
         }
     }
 
@@ -85,46 +82,48 @@ class MainActivity : AppCompatActivity() {
         this.startActivity(intent)
     }
 
-    // RecyclerView.ViewHolderを継承した自作ViewHolder
-    // 親クラスの初期化にはBinding.rootで親Viewを渡し、
-    // 子クラスはbindingを保持する
-    class PhotoCardDataViewHolder(val binding: PhotoCardBinding) : RecyclerView.ViewHolder(binding.root)
 
-    class MyRecycleAdapter(private val context: Context) : RecyclerView.Adapter<PhotoCardDataViewHolder>() {
-        var onClick:((data:PhotoData)->Unit)? = null
-        var list:List<PhotoData> = emptyList()
-        // TODO DiffUtilでいい感じに差分更新してくれるみたい。
-        // https://qiita.com/Tsutou/items/69a28ebbd69b69e51703
-        private val layoutInflater = LayoutInflater.from(context)
+}
 
-        override fun getItemCount(): Int {
-            return list.size
-        }
+// RecyclerView.ViewHolderを継承した自作ViewHolder
+// 親クラスの初期化にはBinding.rootで親Viewを渡し、
+// 子クラスはbindingを保持する
+class PhotoCardDataViewHolder(val binding: PhotoCardBinding) : RecyclerView.ViewHolder(binding.root)
 
-        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PhotoCardDataViewHolder {
-            val binding = PhotoCardBinding.inflate(
-                LayoutInflater.from(parent.context),
-                parent,
-                false)
-            // クリックリスナを搭載
-            val viewHolder = PhotoCardDataViewHolder(binding)
-            onClick?.let { callback ->
-                binding.root.setOnClickListener{
-                    val position = viewHolder.adapterPosition // positionを取得
-                    val data = list[position]
-                    Log.d("setOnClickListener","$position")
-                    callback(data)
-                }
+class ItemAdapter(private val context: Context) : RecyclerView.Adapter<PhotoCardDataViewHolder>() {
+    var onClick:((data:PhotoData)->Unit)? = null
+    var list:List<PhotoData> = emptyList()
+    // TODO DiffUtilでいい感じに差分更新してくれるみたい。
+    // https://qiita.com/Tsutou/items/69a28ebbd69b69e51703
+    private val layoutInflater = LayoutInflater.from(context)
+
+    override fun getItemCount(): Int {
+        return list.size
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PhotoCardDataViewHolder {
+        val binding = PhotoCardBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,
+            false)
+        // クリックリスナを搭載
+        val viewHolder = PhotoCardDataViewHolder(binding)
+        onClick?.let { callback ->
+            binding.root.setOnClickListener{
+                val position = viewHolder.adapterPosition // positionを取得
+                val data = list[position]
+                Log.d("setOnClickListener","$position")
+                callback(data)
             }
-            return viewHolder
         }
+        return viewHolder
+    }
 
-        override fun onBindViewHolder(holder: PhotoCardDataViewHolder, position: Int) {
-            // ここではモデルに値をセットしている
-            // →DataBindingにより、自動でViewに反映される
-            holder.binding.date = list[position].dateString
-            holder.binding.visibleLocationIcon = list[position].loc != null
-            Glide.with(context).load(list[position].uri).into(holder.binding.imageView)
-        }
+    override fun onBindViewHolder(holder: PhotoCardDataViewHolder, position: Int) {
+        // ここではモデルに値をセットしている
+        // →DataBindingにより、自動でViewに反映される
+        holder.binding.date = list[position].dateString
+        holder.binding.visibleLocationIcon = list[position].loc != null
+        Glide.with(context).load(list[position].uri).into(holder.binding.imageView)
     }
 }
